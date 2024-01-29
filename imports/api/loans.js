69ce835dcae842b5d7945aa15c8885ced8d53d48
Balaji S V <86550781+Balaji-S-V@users.noreply.github.com>
2024-01-29 14:18:40 +0530

    role crt

---
+++ imports/api/loans.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Loans = new Mongo.Collection('loans');
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { Roles } from 'meteor/alanning:roles'
 
 async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
@@ -7,31 +8,33 @@ async function insertLink({ title, url }) {
 
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://react-tutorial.meteor.com/simple-todos/01-creating-app.html',
-    });
+  Roles.createRole('admin');
+  Roles.createRole('borrower');
+  Roles.createRole('lender');
+  // if (await LinksCollection.find().countAsync() === 0) {
 
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
+    // await insertLink({
+    //   title: 'Do the Tutorial',
+    //   url: 'https://react-tutorial.meteor.com/simple-todos/01-creating-app.html',
+    // });
 
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
+    // await insertLink({
+    //   title: 'Follow the Guide',
+    //   url: 'https://guide.meteor.com',
+    // });
 
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    });
-  }
+    // await insertLink({
+    //   title: 'Read the Docs',
+    //   url: 'https://docs.meteor.com',
+    // });
+
+    // await insertLink({
+    //   title: 'Discussions',
+    //   url: 'https://forums.meteor.com',
+    // });
+  });
 
   // We publish the entire Links collection to all clients.
   // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
-    return LinksCollection.find();
-  });
-});
+  // Meteor.publish("links", function () {
+  //   return LinksCollection.find();
